3d19738670f817f33af751da3d3b412355821e76
Hackkill <theiviboss083@gmail.com>
2023-12-02 13:54:02 +0100

    Reading CONTENT_RAM and storing it with dynamic memory + ParseAdress function improvement

--- MEMsym.c
+++ MEMsym.c
@@ -18,48 +18,7 @@ int failNo = 0;
 typedef struct {
     unsigned char ETQ;
     unsigned char Data[TAM_LINEA];
-    // unsigned char *Data = (unsigned char *) malloc(sizeof(unsigned char) * TAM_LINEA); //Preguntar al Profe si hay q dejarlo como en el PDF o lo modificamos para la MemDyn
 } T_CACHE_LINE;
-
-// Counts no. of lines in a file
-int noLines(FILE *fp) {
-    int cont = 0;
-    char c;
-
-    while(!feof(fp)) {
-        if((c = getc(fp)) == '\n') {
-            cont++;
-        } // end if condition
-    } // end while loop
-
-    return cont;
-} // end of noLines
-
-// Reads a dynamic line of a file
-char * leeLineDinamicaFichero(FILE *f) {
-	char * linea = (char *) malloc(sizeof(char) * 3);
-	int idx = 0;
-	char c = 0;
-	
-    // Stores each character in linea
-	do {
-		//linea = (char*)realloc(linea,sizeof(char)*idx+1);
-		c = getc(f);
-		linea[idx] = c;
-		idx++;
-
-        // Reallocation of memory
-        if((idx + 1) % 3 == 0) {
-            linea = (char *) realloc(linea, sizeof(char) * (idx + 4));
-        } // end if condition
-	} while(!feof(f) && c != '\n'); // end do while
-	
-	if(c == '\n') { // Detects a change of line
-		linea[idx-1] = '\0';
-	} // end if condition
-
-	return linea;
-} // end of leeLineaDinamicaFichero
 
 // Initializes the label fields to xFF and cache data to x23
 void cleanCache(T_CACHE_LINE *tbl) {
@@ -86,30 +45,24 @@ void dumpCache(T_CACHE_LINE *tbl) {
     } // end for loop
 } // end of dumpCache
 
-void parseAddress(unsigned int addr, int *LABEL, int *word, int *line, int *block) {
-    int i = 0;
-
+/*void parseAddress(unsigned int addr, int *LABEL, int *word, int *line, int *block) {
     *block = addr & 0x0F; // Extract the 4-bit block field
     *word = (addr >> 4) & 0x07; // Extract the 3-bit word field
     *line = (addr >> 7) & 0x07; // Extract the 3-bit line field
     *LABEL = (addr >> 10) & 0x1F; // Extract the 5-bit label field
-
-    if (*LABEL != cache[*line].ETQ) {
-        printf("T: %d, Cache Miss %d, Address %04X, Label %X, Line %02X, Word %02X, Block %02X", i++, failNo++, addr, *LABEL, *line, *word, *block);
-    } else {
-        printf("T: %d, Cache Hit, Address %04X, Label %X, Line %02X, Word %02X, Data %02X", i++, addr, *LABEL, *line, *word, cache[*line].Data[*word]);
-    }
-} // end of parseAddress
+} // end of parseAddress*/
 
 void treatFailureMiss(T_CACHE_LINE *tbl, char *MRAM, int LABEL, int line, int block) {} // end of treatFailureMiss
 
 int main() {
     // Initialization of variables
     char *linea_buff;
-    //char *accesos = (char *) malloc(sizeof(char) * 10); // Array of memory accesses
-    FILE *memoryA = fopen("accesos_memoria.txt", "r"); // Opens accesos_memoria.txt in read mode
-    FILE *ramC = fopen("CONTENTS_RAM.bin", "r"); // Opens CONTENTS_RAM.bin in read mode
+    FILE *memoryA = fopen("./accesos_memoria.txt", "r"); // Opens accesos_memoria.txt in read mode
+    FILE *ramC = fopen("./CONTENTS_RAM.bin", "rb"); // Opens CONTENTS_RAM.bin in read mode
+    FILE *cacheC = fopen("./CONTENTS_CACHE.bin", "wb"); // Opens CONTENTS_CACHE.bin in write mode
     T_CACHE_LINE *cacheConts = (T_CACHE_LINE *) malloc(NUM_ROWS * sizeof(T_CACHE_LINE)); // Array of cache lines, remember that NUM_ROWS = 8
+    //unsigned char *simulRAM = (unsigned char *) malloc(sizeof(unsigned char) * 4096); // Array that reads the contents in CONTENTS_RAM.bin
+    int *address = (int *) malloc(sizeof(int) * 5); // Array of division of address
 
     // Returns -1 when at least one of the files is not found
     if(memoryA == NULL) {
@@ -118,7 +71,10 @@ int main() {
     } else if(ramC == NULL) {
         printf("Could not open 'CONTENTS_RAM.bin'. \n");
         return -1;
-    } // end if, else if conditions
+    } else if(cacheC == NULL) {
+        printf("Could not open 'CONTENTS_CACHE.bin'. \n");
+        return -1;
+    } // end if, else if, else if conditions
 
     // Initializes cache contents
     cleanCache(cacheConts);
@@ -126,39 +82,55 @@ int main() {
     // Dump the contents of the cache on the screen
     dumpCache(cacheConts);
 
-    /*for(int i = 0; i < NUM_ROWS; i++) {
-        initialize(*(linea_buff + i)); // Invokes the function to initialize the label fields
-    } // end for loop*/
+    sleep(1); // 1 second sleep
     
-    /*for(int i = 0; i < noLines(memoryA); i++) {
-        linea_buff = leeLineDinamicaFichero(memoryA);
-        *(accesos + i) = linea_buff;
+    unsigned char *Simul_RAM = (unsigned char*) malloc(sizeof(unsigned char) * 4096);
+    if (Simul_RAM == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return -1;
+    } // end if condition
 
-        if((i + 1) % 10 == 0) {
-            accesos = (char *) realloc(accesos, sizeof(char) * (i + 11));
-        } // end if condition
-    } // end for loop*/
+    fread(Simul_RAM, sizeof(unsigned char), 4096, ramC);
 
+    /*//PRUEBA
+    for (int i=0; i<linesInMemoryA; i++) {
+        printf("%02X \n", Simul_RAM[i]);
+    }
+    printf("\n");*/ 
+
+    // address[0] = linea i de accesos memoria;
+    //parseAddress(address[0], address[1], address[2], address[3], address[4]); // address[1-4] are empty and ready to be substituted
+    
+    /*
+    //Print Miss or Hit
+    printf("T: %d, Cache Miss %d, Address %04X, Label %X, Line %02X, Word %02X, Block %02X", i++, failNo++, addr, *LABEL, *line, *word, *block);
+    printf("T: %d, Cache Hit, Address %04X, Label %X, Line %02X, Word %02X, Data %02X", i++, addr, *LABEL, *line, *word, <ALGO>);
+    */
+   
     // Calculates the cache line of a given address
 
 
     // Checks if tag cache line contains is the same
 
 
-    sleep(1); // 1 second sleep
-
     // Writes the number of accesses and failures, avg time and text read
 
 
     // Process ends after reading dirs_memoria.txt (accesos_memoria.txt???) and actions needed
 
+
+    // Dump contents of information into CONTENTS_CACHE.bin
+
+
     // Closes the files
     fclose(memoryA);
     fclose(ramC);
+    fclose(cacheC);
 
     // Frees dynamic memory allocations
     free(linea_buff);
     free(cacheConts);
+    free(Simul_RAM);
 
     printf("Al menos esto se esta imprimiendo. \n");
 
